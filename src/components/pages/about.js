a31e7acdbdbbafeb6e0e3a53a4b0964ecf2e32ad
AdamLind <adamsplind@gmail.com>
2022-03-31 21:22:12 -0600

    no pic

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,17 +1,9 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
 
 export default function() {
   return (
     <div className="content-page-wrapper">
-      <div
-        className="left-column"
-        style={{
-          background: 'url(' + profilePicture + ') no-repeat',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}  
-      />
+
       <div className="right-column">
         Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis
         dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis.
