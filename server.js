f6eb534502149e4c8900a9ed95e6b95c413f8669
Adam Lind <adamsplind@gmail.com>
2023-09-22 20:26:15 -0600

    changed port specs

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 })
-app.listen(port);
+app.listen(port, () => console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env));
 
 console.log("server started");
